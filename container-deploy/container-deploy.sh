3076ef501611cebba181caf0da16518c7eac21bd
Ben Schwartz <ben.schwartz@bancvue.com>
2013-09-23 10:21:23 -0500

    container deploy script

---
+++ container-deploy/container-deploy.sh
@@ -0,0 +1,22 @@
+#/bin/bash
+
+WILDCARD_NAME="io.fliglio.com"
+
+TAG=$1  # jbfink/wordpress:latest
+
+
+NAME=$(echo $TAG | awk -F':' '{print $1}') # jbfink/wordpress
+
+CLEAN_NAME=$(echo $NAME | tr / -) # jbfink-wordpress
+
+
+CONTAINER_ID=$(sudo docker run -d $TAG)
+CONTAINER_PORT=$(sudo docker port $CONTAINER_ID 80)
+CONTAINER_BRIDGE=$(sudo docker inspect $CONTAINER_ID | grep Bridge | cut -d":" -f2 | cut -d'"' -f2)
+CONTAINER_IP=$(/sbin/ifconfig  $CONTAINER_BRIDGE | sed -n '2 p' | awk '{print $2}' | cut -d":" -f2)
+
+REDIS_HOST="$IP" # its all the same thing on one node
+REDIS_PORT="6379"
+
+redis-cli -h $REDIS_HOST -p $REDIS_PORT rpush frontend:$CLEAN_NAME.$WILDCARD_NAME $CLEAN_NAME
+redis-cli -h $REDIS_HOST -p $REDIS_PORT rpush frontend:$CLEAN_NAME.$WILDCARD_NAME http://$CONTAINER_IP:$CONTAINER_PORT
