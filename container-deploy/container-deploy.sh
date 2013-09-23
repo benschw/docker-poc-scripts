c912cf5ccea114c01686da17a6255ffd8adec370
Ben Schwartz <ben.schwartz@bancvue.com>
2013-09-23 14:49:18 -0500

    container deploy script

--- container-deploy/container-deploy.sh
+++ container-deploy/container-deploy.sh
@@ -21,7 +21,7 @@ REDIS_PORT="6379"
 CONTAINERS=$(docker ps  | grep $REPO | awk '{print $1}' | sed ':a;N;$!ba;s/\n/ /g')
 if [ -n CONTAINERS ]
 then
-	echo a$CONTAINERSa
+	echo a$CONTAINERS a
 	docker stop $CONTAINERS > /dev/null
 fi
 
